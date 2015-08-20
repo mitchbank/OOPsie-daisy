a13178021fddaa6d35fcca9e8805b8aea0050683
Mitchell Bank <mitchbank26@gmail.com>
2015-08-20 14:21:15 -0400

    called learn and teach methods

--- people.rb
+++ people.rb
@@ -1,12 +1,12 @@
 class Person
 
 	def initialize(first_name, last_name)
-		@@first_name = first_name
-		@@last_name = last_name
+		@first_name = first_name
+		@last_name = last_name
 	end
 
 	def to_s
-		"Hi, my name is #{@@first_name} #{@@last_name}"
+		"Hi, my name is #{@first_name} #{@last_name}"
 	end
 end
 
@@ -31,3 +31,12 @@ puts student
 teacher = Teacher.new("Chris", "Bale")
 
 puts teacher
+
+puts student.learn
+
+puts teacher.teach
+
+# if we were to try and call the learn methond for the teacher
+# or the teach method for the student, it wouldn't work
+# because the student class cannot call a method from the 
+# teacher class and vice versa
