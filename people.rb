c2d612fea0f73fafc85a82cee19a7856ff3bd9d2
Mitchell Bank <mitchbank26@gmail.com>
2015-08-20 13:48:45 -0400

    added output for teach. Ammended code for learn

--- people.rb
+++ people.rb
@@ -4,13 +4,27 @@ class Student
 		"I get it!"
 	end
 
+	def to_s
+		learn
+	end
+
 end
 
 
 class Teacher
+	def teach
+		"Everything in Ruby is and Object"
+	end
 
+	def to_s
+		teach		
+	end
 end
 
 student = Student.new
 
 puts student
+
+teacher = Teacher.new
+
+puts teacher
