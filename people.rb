b54fbd0bec31027cfac7978bef04f215d7f27c52
Mitchell Bank <mitchbank26@gmail.com>
2015-08-20 14:07:01 -0400

    added greeting

--- people.rb
+++ people.rb
@@ -1,37 +1,38 @@
 class Person
 
-	def initialize(name)
-		@@name = name
-
+	def initialize(first_name, last_name)
+		@@first_name = first_name
+		@@last_name = last_name
+	end
 end
 
-class Student
+class Student < Person
 
 	def learn
 		"I get it!"
 	end
 
 	def to_s
-		learn
+		"Hi, my name is #{@@first_name} #{@@last_name}"
 	end
 
 end
 
 
-class Teacher
+class Teacher < Person
 	def teach
 		"Everything in Ruby is and Object"
 	end
 
 	def to_s
-		teach		
+		"Hi, my name is #{@@first_name} #{@@last_name}"
 	end
 end
 
-student = Student.new
+student = Student.new("Keener", "Studint")
 
 puts student
 
-teacher = Teacher.new
+teacher = Teacher.new("Linus", "Techtips")
 
 puts teacher
